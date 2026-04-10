ccf0c2e5298633ab01733b32489d28c5db24236d
AaronMartinez91 <aaronalejandro2@gmail.com>
2026-04-10 11:33:40 +0200

    Cambios estructurales importantes: -creamos carpeta algorithms en services -cada algoritmo tendrá su propio script y se cambiará todo el uso de la app en función del escogido en config.py -cambios realizados en entropyService para compatibilizar estos cambios

---
+++ backend/services/algorithms/v0_1_0.py
@@ -0,0 +1,9 @@
+# services/algorithms/v0_1_0.py
+import hashlib
+import numpy as np
+
+def extraer_entropia(frame1, frame2):
+    bytes1 = np.frombuffer(frame1.raw_bytes, dtype=np.uint8)
+    bytes2 = np.frombuffer(frame2.raw_bytes, dtype=np.uint8)
+    diferencia = np.abs(bytes1.astype(np.int16) - bytes2.astype(np.int16)).astype(np.uint8)
+    return hashlib.sha256(diferencia.tobytes()).digest()
--- backend/services/entropy_service.py
+++ backend/services/entropy_service.py
@@ -1,31 +1,22 @@
-import hashlib
-import numpy as np
-from infrastructure.camera_reader import CameraReader
+import importlib
+from config import ALGORITHM_VERSION
 
 class EntropyService:
 
     def __init__(self, camera_reader):
         self.camera_reader = camera_reader
         self.previous_frame = None
+        version_module = ALGORITHM_VERSION.replace(".", "_")
+        modulo = importlib.import_module(f"services.algorithms.v{version_module}")
+        self.extraer_entropia = modulo.extraer_entropia
 
     def extract_entropy(self):
         if self.previous_frame is None:
             frame1 = self.camera_reader.read_frame()
         else:
             frame1 = self.previous_frame
-        
+
         frame2 = self.camera_reader.read_frame()
         self.previous_frame = frame2
 
-        bytes1 = np.frombuffer(frame1.raw_bytes, dtype=np.uint8)
-        bytes2 = np.frombuffer(frame2.raw_bytes, dtype=np.uint8)
-
-        diferencia = np.abs(bytes1.astype(np.int16) - bytes2.astype(np.int16)).astype(np.uint8)
-
-        entropia = hashlib.sha256(diferencia.tobytes()).digest()
-
-        # importante! hashlib.sha256 devuelve 32 bits, pero solo usaremos 16. Esto está pensado para poder usar en el futuro hashes más completos,
-        # y es como funciona esta librería en concreto.
-
-        return entropia
-
+        return self.extraer_entropia(frame1, frame2)
